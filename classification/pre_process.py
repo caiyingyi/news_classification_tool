5843c15e540c1322782a04fdd310c1ad5021a00b
caiyingyi <caiyingyi902@163.com>
2018-04-07 09:52:04 +0800

    分类：添加判断，语料是否已经预处理

--- classification/pre_process.py
+++ classification/pre_process.py
@@ -19,36 +19,39 @@ class PreProcess(object):
     def data_filter(self):
         self.cursor.rewind()
         for review in self.cursor:
-            data = review["content"]
+            if review.get("filter_data"):
+                continue
+            else:
+                data = review["content"]
 
-            # 去除html标签
-            re_cdata = re.compile('//<!\[CDATA\[[^>]*//\]\]>', re.I)  # 匹配CDATA
-            re_script = re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>', re.I)  # Script
-            re_style = re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', re.I)  # style
-            re_br = re.compile('<br\s*?/?>')  # 处理换行
-            re_h = re.compile('</?\w+[^>]*>')  # HTML标签
-            re_comment = re.compile('<!--[^>]*-->')  # HTML注释
-            re_blank_line = re.compile('\n+')  # 去掉多余的空行
-            re_charentity = re.compile(r'&#?(?P<name>\w+);')  # 去掉特殊字符
+                # 去除html标签
+                re_cdata = re.compile('//<!\[CDATA\[[^>]*//\]\]>', re.I)  # 匹配CDATA
+                re_script = re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>', re.I)  # Script
+                re_style = re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', re.I)  # style
+                re_br = re.compile('<br\s*?/?>')  # 处理换行
+                re_h = re.compile('</?\w+[^>]*>')  # HTML标签
+                re_comment = re.compile('<!--[^>]*-->')  # HTML注释
+                re_blank_line = re.compile('\n+')  # 去掉多余的空行
+                re_charentity = re.compile(r'&#?(?P<name>\w+);')  # 去掉特殊字符
 
-            s = re_cdata.sub('', data)  # 去掉CDATA
-            s = re_script.sub('', s)  # 去掉SCRIPT
-            s = re_style.sub('', s)  # 去掉style
-            s = re_br.sub('', s)  # 去掉br
-            s = re_h.sub('', s)  # 去掉HTML 标签
-            s = re_comment.sub('', s)  # 去掉HTML注释
-            s = re_blank_line.sub('\n', s)  # 去掉多余的空行
-            s = re_charentity.sub("", s)  # 去掉特殊字符
+                s = re_cdata.sub('', data)  # 去掉CDATA
+                s = re_script.sub('', s)  # 去掉SCRIPT
+                s = re_style.sub('', s)  # 去掉style
+                s = re_br.sub('', s)  # 去掉br
+                s = re_h.sub('', s)  # 去掉HTML 标签
+                s = re_comment.sub('', s)  # 去掉HTML注释
+                s = re_blank_line.sub('\n', s)  # 去掉多余的空行
+                s = re_charentity.sub("", s)  # 去掉特殊字符
 
-            # nltk分词
-            data = [word.lower() for word in word_tokenize(s)]
-            # nltk去停词,去标点
-            english_stopwords = stopwords.words('english')
-            english_punctuations = [',', '.', ':', ';', '?', '(', ')', '[', ']', '&', '!', '*', '@', '#', '$', '%']
-            data = [word for word in data if word not in english_stopwords + english_punctuations]
-            # nltk 词干化
-            st = LancasterStemmer()
-            data = [st.stem(word) for word in data]
+                # nltk分词
+                data = [word.lower() for word in word_tokenize(s)]
+                # nltk去停词,去标点
+                english_stopwords = stopwords.words('english')
+                english_punctuations = [',', '.', ':', ';', '?', '(', ')', '[', ']', '&', '!', '*', '@', '#', '$', '%']
+                data = [word for word in data if word not in english_stopwords + english_punctuations]
+                # nltk 词干化
+                st = LancasterStemmer()
+                data = [st.stem(word) for word in data]
 
-            # 插入数据库
-            self.collection.update({"_id": review['_id']}, {"$set": {"filter_data": data}})
+                # 插入数据库
+                self.collection.update({"_id": review['_id']}, {"$set": {"filter_data": data}})
