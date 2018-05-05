c049df7c0e1e39965352e12a5a110b4a3520afe3
caiyingyi <caiyingyi902@163.com>
2018-05-05 15:52:05 +0800

    finished spiders

--- ennews_spider/run.py
+++ ennews_spider/run.py
@@ -3,5 +3,6 @@
 from scrapy import cmdline
 
 if __name__ == '__main__':
+    # spiders_names = ["bbc", "foxnews", "news_asia", "newsweek", "upi"]
     name = "upi"
     cmdline.execute(("scrapy crawl %s" % name).split())
